859fdacfbd1b8ecd5253a49fe46847c9afc9ee10
Austin <christopheraustinwallace@gmail.com>
2023-11-08 09:18:26 -0500

    add operate function in js
    added the operate function, which takes in nums a and b and an operation then calls the operation function on those numbers

--- javascript.js
+++ javascript.js
@@ -20,3 +20,25 @@ function exponent(a, b) {
 // console.log(multiply(2, 2))
 // console.log(divide(2, 2))
 // console.log(exponent(2, 2))
+
+// 2) create 3 variables for a number, operator, another number. 
+// (each part of a calculator operation)
+let first_num;
+let second_num;
+let operation;
+
+// 3) create a function called operate that takes an operator and 2 numbers and then calls
+// one of the above functions on the numbers
+function operate(a, b, operation) {
+    if (operation == '+') {
+        add(a, b)
+    } else if (operation == '-') {
+        subtract(a, b)
+    } else if (operation == '*') {
+        multiply(a, b)
+    } else if (operation == '/') {
+        divide(a, b)
+    } else if (operation == '**') {
+        exponent(a, b)
+    }
+}
