215eba1a78ade5a548d81c6993e107dd8f066447
Austin <christopheraustinwallace@gmail.com>
2023-11-08 08:34:30 -0500

    create functions for basic math operations
    created javascript functions for basic math operations: add, subtract, multiply, divide, exponentiate

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// 1) create functions for the basic math operations: add, subtract, multiply, divide, exponent
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b
+}
+function divide(a, b) {
+    return a / b
+}
+function exponent(a, b) {
+    return a ** b
+}
+// testing the basic math operations:
+// console.log(add(2, 2))
+// console.log(subtract(2, 2))
+// console.log(multiply(2, 2))
+// console.log(divide(2, 2))
+// console.log(exponent(2, 2))
